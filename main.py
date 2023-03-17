9979099ca2e3722d43e31c01a7a017dd7ae30303
Danilo Bruno <danilobsilv@gmail.com>
2023-03-16 22:30:20 -0400

    novos testes realizados

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from serializer.serializer import Serializer
 from models.cliente import Cliente
 from models.funcionario import Funcionario
 from models.item import Item
+from controllers.controller_login import LoginController
 
 lista = []
 serial = Serializer()
@@ -13,19 +14,22 @@ serial = Serializer()
 # for client in lista:
 #     serial.serializeClient(client)
 
-# func1 = Funcionario("funcionario 1","123456789","819273",100.21, "10/05/2050")
+func1 = Funcionario("funcionario 1","123456789","819273",100.21, "10/05/2050")
 # lista.append(func1)
-# func2 = Funcionario("funcionario 2", "098080","09808098123",109.90,"17/04/2025")
+func2 = Funcionario("funcionario 2", "098080","09808098123",109.90,"17/04/2025")
 # lista.append(func2)
 
 # for func in lista:
 #     serial.serializeFunctionary(func)
 
-item1 = Item("Coca-cola",5.50, 20)
-lista.append(item1)
-item2 = Item("Pizza calabresa", 22.50, 50)
-lista.append(item2)
+# item1 = Item("Coca-cola",5.50, 20)
+# lista.append(item1)
+# item2 = Item("Pizza calabresa", 22.50, 50)
+# lista.append(item2)
 
-for elem in lista:
-    serial.serializeItem(elem)
+# for elem in lista:
+#     serial.serializeItem(elem)
     
+cont = LoginController()
+cont.validateFunctionary(func1)
+cont.validateFunctionary(func2)
