46ffd55c2a43a99adf190777c0faf4349de9e783
Danilo Bruno <danilobsilv@gmail.com>
2023-03-16 20:34:48 -0400

    fiz o serializador de itens

--- serializer/serializer.py
+++ serializer/serializer.py
@@ -5,16 +5,16 @@ import jsons
 
 class Serializer:
       def __init__(self):
-            # self.file_name_client = "mps_pizzaria\jsons\clients.json"
-            # self.file_name_functionary = "mps_pizzaria\jsons\osfuncionarios.json"
-            # self.counter = 1
-            pass
+            self.file_name_client = "mps_pizzaria\jsons\_client.json"
+            self.file_name_functionary = "mps_pizzaria\jsons\_funcionary.json"
+            self.file_name_item_menu = "mps_pizzaria\jsons\_itemMenu.json"
+            self.file_name_stock = "mps_pizzaria\jsons\_stock.json"
             
 
       def serializeClient(self, client:object):
             
             data = {}
-            with open("mps_pizzaria\jsons\clients.json", "r") as outfile:
+            with open(self.file_name_client, "r") as outfile:
                   try:
                         data = json.load(outfile)
                   except:
@@ -24,12 +24,13 @@ class Serializer:
                   dictionary = {"name":client.name, "password":client.password, "rg":client.rg, "phone number":client.phone_number, "gender":client.gender}
                   data[client.id] = dictionary
                   
-                  with open("mps_pizzaria\jsons\clients.json", "w") as outfile:
+                  with open(self.file_name_client, "w") as outfile:
                         json.dump(data, outfile, indent=4)             
 
+      
       def serializeFunctionary(self, functionary:object):
             data = {}
-            with open("mps_pizzaria\jsons\osfuncionarios.json", "r") as outfile:
+            with open(self.file_name_functionary, "r") as outfile:
                   try:
                         data = json.load(outfile)
                   except:
@@ -38,6 +39,21 @@ class Serializer:
                   dictionary = {"name": functionary.name, "password":functionary.password, "rg":functionary.rg, "salary":functionary.salary, "hiring date": functionary.hiring_date}
                   data[functionary.id] = dictionary
 
-                  with open("mps_pizzaria\jsons\osfuncionarios.json", "w") as outfile:
+                  with open(self.file_name_functionary, "w") as outfile:
                         json.dump(data, outfile, indent=4)
                         
+
+      def serializeItem(self, item):
+            data = {}
+            with open(self.file_name_stock, "r") as outfile:
+                  try:
+                        data = json.load(outfile)
+                  except:
+                        raise Invalid("ERRO")
+                  
+                  dictionary = {"price": item.price, "amount": item.amount}
+                  data[item.product] = dictionary
+
+                  with open(self.file_name_stock, "w") as outfile:
+                        json.dump(data, outfile, indent=4)
+            
