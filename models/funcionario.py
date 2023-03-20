349933ef45557f02c4afeec6b3901ba687459cd3
Danilo Bruno <danilobsilv@gmail.com>
2023-03-19 22:34:09 -0400

    método create implementado, delete a ser feito

--- models/funcionario.py
+++ models/funcionario.py
@@ -1,9 +1,10 @@
 from models.usuario import Usuario
-from datetime import date
 from random import randint
+from serializer.serializer import Serializer
+import json
 
 class Funcionario(Usuario): #id, salary, float, hiring_date
-    def __init__(self, name: str, password: str, rg: str, cargo, salary:float, hiring_date:date):
+    def __init__(self, name: str, password: str, rg: str, cargo, salary:float, hiring_date):
         super().__init__(name, password, rg, cargo)
         self.id = randint(100,999)
         self.salary = salary
@@ -26,5 +27,23 @@ class Funcionario(Usuario): #id, salary, float, hiring_date
     
     def checkFuncionario(self):  # esse método vai validar se um funcionário existe ou não, irá validá-lo
         pass
+    
+    def createFuncionario(self, name, password, rg, cargo, salary, hiring_date):
+        new_funcionary = Funcionario(name, password, rg, cargo, salary, hiring_date)
+        serializer = Serializer()
+        serializer.serializeFunctionary(new_funcionary)
+
+    def deleteFuncionario(self, funcionary):
+        try:
+            with open("mps_pizzaria\jsons\_funcionary.json", "r") as outfile:
+                data = json.load(outfile)
+        except:
+            raise Exception("ERROR: Could not open the file.")
+        
+        del data[funcionary]
+
+        with open("mps_pizzaria\jsons\_funcionary.json", "w") as outfile:
+            json.dump(data, outfile)
+
 
     
