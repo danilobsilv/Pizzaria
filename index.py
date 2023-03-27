0c5c3d6d0b3466b651a69b061126aa1d6dd9d111
Danilo Bruno <danilobsilv@gmail.com>
2023-03-27 18:21:03 -0400

    tirando comentários

--- index.py
+++ index.py
@@ -2,17 +2,6 @@ import sys
 from PySide2.QtWidgets import QApplication, QMainWindow
 from gui.login_screen    import UI_LoginScreen
 
-
-# from serializer.serializer import  Serializer
-# from models.item import Item
-
-# serial = Serializer()
-# item = Item("calabresa",22.50, 10)
-# item1 = Item("coca-cola",10,15)
-# serial.serializeItem(item)
-# serial.serializeItem(item1)
-
-
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainWindow = QMainWindow()
